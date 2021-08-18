610045e443422c40d0ad8a9d63aa0dc5d964adce
RajeshRaju2 <rajeshbandi2180@gmail.com>
2021-08-18 10:24:58 +0530

    Adding User Files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { AppComponent } from './app.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'home',component:HomeComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'aboutus',component:AboutusComponent},
+  {path:'contactus',component:ContactusComponent},
+  {path:'profile',component:ProfileComponent},
+  {path:'app-root',component:AppComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
