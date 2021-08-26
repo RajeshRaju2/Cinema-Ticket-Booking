0646795be3d713106e25ddd949982810dfad2b4e
RajeshRaju2 <rajeshbandi2180@gmail.com>
2021-08-26 12:51:07 +0530

    Updating all backend files

---
+++ cinematicket/src/main/java/com/mph/entity/User.java
@@ -0,0 +1,111 @@
+package com.mph.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+private int userId;
+	@Column
+private String fname;
+	@Column
+private String lname;
+	@Column
+private String gender;
+	@Column
+private String email;
+	@Column
+private String password;
+	@Column
+private String phone;
+
+public User() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+public User(int userId,String fname, String lname, String gender, String email, String password, String phone) {
+	super();
+	this.userId = userId;
+	this.fname = fname;
+	this.lname = lname;
+	this.gender = gender;
+	this.email = email;
+	this.password = password;
+	this.phone = phone;
+}
+
+public int getUserId() {
+	return userId;
+}
+
+public void setUserId(int userId) {
+	this.userId = userId;
+}
+
+public String getFname() {
+	return fname;
+}
+
+public void setFname(String fname) {
+	this.fname = fname;
+}
+
+public String getLname() {
+	return lname;
+}
+
+public void setLname(String lname) {
+	this.lname = lname;
+}
+
+public String getGender() {
+	return gender;
+}
+
+public void setGender(String gender) {
+	this.gender = gender;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+public String getPhone() {
+	return phone;
+}
+
+public void setPhone(String phone) {
+	this.phone = phone;
+}
+
+@Override
+public String toString() {
+	return "User [userId=" + userId + ", fname=" + fname + ", lname=" + lname + ", gender=" + gender + ", email="
+			+ email + ", password=" + password + ", phone=" + phone + "]";
+}
+
+
+
+
+}
